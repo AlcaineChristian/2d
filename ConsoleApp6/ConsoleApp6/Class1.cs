52a4552fc65ad2aad115210c59c45783b49d5d13
Alcaine Tomas <40808690+AlcaineChristian@users.noreply.github.com>
2019-03-25 21:55:55 -0300

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Alumno
+    {
+        byte nota1;
+        byte nota2;
+        float notaFinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+        public static string colegio;
+
+        public Alumno(string Apellido, string Nombre, int Legajo)
+        {
+            this.nombre = Nombre;
+            this.apellido = Apellido;
+            this.legajo = Legajo;
+            this.notaFinal = -1;
+        }
+
+        static Alumno()
+        {
+            colegio = "A.T.R";
+
+        }
+
+        public void CalcularFinal()
+        {
+            if(this.nota1>4 && this.nota2>4 )
+            {
+                this.notaFinal = (this.nota1 + this.nota2) / 2;
+            }
+
+        }
+        public void Estudiar(byte notaUno, byte notaDos)
+        {
+            this.nota1 = notaUno;
+            this.nota2 = notaDos;
+
+        }
+        public string Mostrar()
+        {
+            return this.nombre + " " + this.apellido + " - " + legajo + " - " + colegio + " - " + (this.notaFinal == -1 ? "DESAPROBADO" : this.notaFinal.ToString());
+        }
+
+
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno("Alcaine", "Tomas",1848);
+            Alumno alumno2 = new Alumno("Garaban", "Cristian",1849);
+            Alumno alumno3 = new Alumno("FE", "FA",1850);
+            //Alumno.colegio = "TSP";
+            alumno1.Estudiar(6, 9);
+            alumno1.CalcularFinal();
+            alumno2.Estudiar(3, 9);
+            alumno2.CalcularFinal();
+            alumno2.Estudiar(6, 2);
+            alumno2.CalcularFinal();
+
+
+            Console.WriteLine(alumno1.Mostrar());
+            Console.WriteLine(alumno2.Mostrar());
+            Console.WriteLine(alumno3.Mostrar());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
